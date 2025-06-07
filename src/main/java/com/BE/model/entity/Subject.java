d5b13a097847842ea1f21dbf41c3a5ed8a00b441
DoMinh-28 <minhlola28@gmail.com>
2025-06-07 15:38:12 +0700

    Feat:Minh Initialize master data entities (Grade, Subject, Book, Chapter, Lesson)

---
+++ src/main/java/com/BE/model/entity/Subject.java
@@ -0,0 +1,20 @@
+package com.BE.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Subject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+}
