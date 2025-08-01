e2d76c1424fdd7d3ad2ee586a08ec2c9c19845cc
DoMinh-28 <minhlola28@gmail.com>
2025-08-01 22:22:53 +0700

    Refactor: Comment out AuthAggregatorController and related service classes

--- PlanBook-Aggregator/src/main/java/com/BE/service/implementServices/AuthAggregatorServiceImpl.java
+++ PlanBook-Aggregator/src/main/java/com/BE/service/implementServices/AuthAggregatorServiceImpl.java
@@ -1,38 +1,38 @@
-package com.BE.service.implementServices;
-
-import com.BE.feign.IdentityServiceClient;
-import com.BE.mapper.AuthAggregatorMapper;
-import com.BE.model.request.AuthenticationRequest;
-import com.BE.model.request.RegisterAggregatorRequest;
-import com.BE.model.request.UserProfileRequest;
-import com.BE.model.response.AuthenticationResponse;
-import com.BE.model.response.DataResponseDTO;
-import com.BE.model.response.UserProfileResponse;
-import com.BE.service.interfaceServices.IAuthAggregatorService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class AuthAggregatorServiceImpl implements IAuthAggregatorService {
-
-    private final IdentityServiceClient authClient;
-//    private final UserServiceClient userClient;
-    private final AuthAggregatorMapper mapper;
-
-    @Override
-    public AuthenticationResponse register(RegisterAggregatorRequest req) {
-        AuthenticationRequest auth = mapper.toAuthenticationRequest(req);
-
-        UserProfileRequest profile = UserProfileRequest.builder()
-                .fullName(req.getFullName())
-                .build();
-
-        DataResponseDTO<AuthenticationResponse> authRes = authClient.register(auth);
-
-//        DataResponseDTO<UserProfileResponse> userProfile = userClient.createProfile(authRes.getData().getId(), profile);
-
-//        authRes.getData().setFullName(userProfile.getData().getFullName());
-        return authRes.getData();
-    }
-}
+//package com.BE.service.implementServices;
+//
+//import com.BE.feign.IdentityServiceClient;
+//import com.BE.mapper.AuthAggregatorMapper;
+//import com.BE.model.request.AuthenticationRequest;
+//import com.BE.model.request.RegisterAggregatorRequest;
+//import com.BE.model.request.UserProfileRequest;
+//import com.BE.model.response.AuthenticationResponse;
+//import com.BE.model.response.DataResponseDTO;
+//import com.BE.model.response.UserProfileResponse;
+//import com.BE.service.interfaceServices.IAuthAggregatorService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class AuthAggregatorServiceImpl implements IAuthAggregatorService {
+//
+//    private final IdentityServiceClient authClient;
+////    private final UserServiceClient userClient;
+//    private final AuthAggregatorMapper mapper;
+//
+//    @Override
+//    public AuthenticationResponse register(RegisterAggregatorRequest req) {
+//        AuthenticationRequest auth = mapper.toAuthenticationRequest(req);
+//
+//        UserProfileRequest profile = UserProfileRequest.builder()
+//                .fullName(req.getFullName())
+//                .build();
+//
+//        DataResponseDTO<AuthenticationResponse> authRes = authClient.register(auth);
+//
+////        DataResponseDTO<UserProfileResponse> userProfile = userClient.createProfile(authRes.getData().getId(), profile);
+//
+////        authRes.getData().setFullName(userProfile.getData().getFullName());
+//        return authRes.getData();
+//    }
+//}
