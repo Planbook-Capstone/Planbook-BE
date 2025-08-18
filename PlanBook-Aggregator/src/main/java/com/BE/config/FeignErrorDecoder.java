75d0b4b087be988f89210375ed886b7538f8eda3
DoMinh-28 <minhlola28@gmail.com>
2025-08-18 20:29:35 +0700

    fix: remove redundant @Component annotations from Feign beans

--- PlanBook-Aggregator/src/main/java/com/BE/config/FeignErrorDecoder.java
+++ PlanBook-Aggregator/src/main/java/com/BE/config/FeignErrorDecoder.java
@@ -6,13 +6,11 @@ import feign.Response;
 import feign.codec.ErrorDecoder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 @Slf4j
-@Component
 public class FeignErrorDecoder implements ErrorDecoder {
 
     private final ErrorDecoder defaultDecoder = new Default();
